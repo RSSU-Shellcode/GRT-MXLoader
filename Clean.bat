498b666ebc1e841613c6671e6fa8b32f47007e51
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-04-16 14:30:04 +0800

    add a clean script for clean Debug and Release output files.

---
+++ Clean.bat
@@ -0,0 +1,13 @@
+rd  /S /Q ".vs\GRT-MXLoader\v17\ipch"
+del /S /Q ".vs\GRT-MXLoader\v17\Browse.VC.db"
+del /S /Q ".vs\GRT-MXLoader\v17\Solution.VC.db"
+
+rd /S /Q "Debug"
+rd /S /Q "Release"
+rd /S /Q "x64"
+rd /S /Q "x86"
+
+rd /S /Q "builder\cs-beacon\Debug"
+rd /S /Q "builder\cs-beacon\Release"
+rd /S /Q "builder\cs-beacon\x64"
+rd /S /Q "builder\cs-beacon\x86"
