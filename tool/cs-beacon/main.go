f80976f58282e64e36bcfdbe2e1498ce1dc5f74c
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-04-16 15:07:10 +0800

    cs-beacon/main.go: add a simple tool for extract stage from stageless beacon.

---
+++ tool/cs-beacon/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/RSSU-Shellcode/GRT-MXLoader/loader"
+)
+
+var (
+	input  string
+	output string
+)
+
+func init() {
+	flag.StringVar(&input, "i", "beacon.exe", "stageless beacon exe file path")
+	flag.StringVar(&output, "o", "stage.dll", "path for save stage dll file")
+	flag.Parse()
+}
+
+func main() {
+	beacon, err := os.ReadFile(input)
+	checkError(err)
+	dll, err := loader.ExtractBeaconStage(beacon)
+	checkError(err)
+	err = os.WriteFile(output, dll, 0600)
+	checkError(err)
+	fmt.Println("extract Cobalt-Strike beacon stage successfully")
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
