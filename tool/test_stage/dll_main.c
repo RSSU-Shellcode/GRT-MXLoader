eca78c0a1e90eac5e85f03939b0a42b8585cf034
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-09-03 13:04:30 +0800

    test_stage/dll_main.c: add DllMain for build a fake entry function.

---
+++ tool/test_stage/dll_main.c
@@ -0,0 +1,18 @@
+﻿#include "c_types.h"
+#include "win_types.h"
+#include "dll_kernel32.h"
+#include "hash_api.h"
+
+// a fake Cobalt-Strike stage for test
+
+#pragma comment(linker, "/ENTRY:DllMain")
+BOOL DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+    if (dwReason == 4)
+    {
+        Sleep_t Sleep = FindAPI_A("kernel32.dll", "Sleep");
+        Sleep(1000);
+        return true;
+    }
+    return false;
+}
