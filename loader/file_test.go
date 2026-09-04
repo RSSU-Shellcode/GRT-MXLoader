07bff531cd0789156ea6a0c860960fff1864d4ea
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-09-04 12:36:38 +0800

    file_test.go: rewrite TestFile, remove TestFileInstance.

--- loader/file_test.go
+++ loader/file_test.go
@@ -1,9 +1,6 @@
 package loader
 
 import (
-	"path/filepath"
-	"runtime"
-	"sync"
 	"testing"
 
 	"github.com/davecgh/go-spew/spew"
@@ -14,82 +11,16 @@ const testFilePath = "C:\\Windows\\System32\\cmd.exe"
 
 func TestFile(t *testing.T) {
 	t.Run("common", func(t *testing.T) {
-		payload := NewFile(testFilePath)
+		image := NewFile(testFilePath)
 
-		config, err := payload.Encode()
+		config, err := image.Encode()
 		require.NoError(t, err)
 
 		spew.Dump(config)
 	})
 
 	t.Run("mode", func(t *testing.T) {
-		payload := NewFile(testFilePath)
-		require.Equal(t, ModeFile, payload.Mode())
+		image := NewFile(testFilePath)
+		require.Equal(t, ModeFile, image.Mode())
 	})
-}
-
-func TestFileInstance(t *testing.T) {
-	if runtime.GOOS != "windows" {
-		return
-	}
-
-	wg := sync.WaitGroup{}
-	t.Run("x86", func(t *testing.T) {
-		if runtime.GOARCH != "386" {
-			return
-		}
-
-		for _, item := range payloads {
-			path, err := filepath.Abs(filepath.Join("../test/payload/x86", item.path))
-			require.NoError(t, err)
-			payload := NewFile(path)
-			opts := &Options{
-				ImageName:    "test.exe",
-				CommandLine:  "-p1 123 -p2 \"hello\"",
-				WaitMain:     item.wait,
-				AllowSkipDLL: true,
-			}
-
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				inst, err := CreateInstance(testLDRx86, 32, payload, opts)
-				require.NoError(t, err)
-
-				addr := loadShellcode(t, inst)
-				ret, _, _ := syscallN(addr)
-				require.NotEqual(t, uintptr(0), ret)
-			}()
-		}
-	})
-
-	t.Run("x64", func(t *testing.T) {
-		if runtime.GOARCH != "amd64" {
-			return
-		}
-
-		for _, item := range payloads {
-			path, err := filepath.Abs(filepath.Join("../test/payload/x64", item.path))
-			require.NoError(t, err)
-			payload := NewFile(path)
-			opts := &Options{
-				ImageName:    "test.exe",
-				CommandLine:  "-p1 123 -p2 \"hello\"",
-				WaitMain:     item.wait,
-				AllowSkipDLL: true,
-			}
-
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				inst, err := CreateInstance(testLDRx64, 64, payload, opts)
-				require.NoError(t, err)
-
-				addr := loadShellcode(t, inst)
-				ret, _, _ := syscallN(addr)
-				require.NotEqual(t, uintptr(0), ret)
-			}()
-		}
-	})
-	wg.Wait()
 }
