d17f0d31969401380b5042bb6cd94bc8d989e682
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-09-04 12:56:59 +0800

    cs-beacon/instance_test.go: add TestCreateInstance and TestEncodeVersion.

--- loader/cs-beacon/instance_test.go
+++ loader/cs-beacon/instance_test.go
@@ -1,1 +1,96 @@
 package beacon
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/RTS-Framework/GRT-Develop/argument"
+	"github.com/RTS-Framework/GRT-MXLoader/loader"
+)
+
+const testFilePath = "stage.dat"
+
+func TestCreateInstance(t *testing.T) {
+	image := loader.NewFile(testFilePath)
+
+	t.Run("x86", func(t *testing.T) {
+		inst, err := CreateInstance("386", image, nil)
+		require.NoError(t, err)
+		require.NotNil(t, inst)
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		inst, err := CreateInstance("amd64", image, nil)
+		require.NoError(t, err)
+		require.NotNil(t, inst)
+	})
+
+	t.Run("custom template", func(t *testing.T) {
+		template, err := os.ReadFile("../../dist/standard/CS_Beacon_x86.bin")
+		require.NoError(t, err)
+		opts := Options{
+			Template: template,
+		}
+
+		inst, err := CreateInstance("386", image, &opts)
+		require.NoError(t, err)
+		require.NotNil(t, inst)
+	})
+
+	t.Run("ignore instantiate options", func(t *testing.T) {
+		template, err := os.ReadFile("../../dist/pipeline/CS_Beacon_x86.bin")
+		require.NoError(t, err)
+		opts := Options{
+			Template:       template,
+			IgnoreInstOpts: true,
+		}
+
+		inst, err := CreateInstance("386", image, &opts)
+		require.NoError(t, err)
+		require.NotNil(t, inst)
+	})
+
+	t.Run("with additional arguments", func(t *testing.T) {
+		t.Run("common", func(t *testing.T) {
+			args := []*argument.Arg{
+				{ID: 100, Data: []byte("config data")},
+			}
+			opts := Options{
+				Arguments: args,
+			}
+
+			inst, err := CreateInstance("386", image, &opts)
+			require.NoError(t, err)
+			require.NotNil(t, inst)
+		})
+
+		t.Run("invalid id", func(t *testing.T) {
+			args := []*argument.Arg{
+				{ID: 1, Data: []byte("config data")},
+			}
+			opts := Options{
+				Arguments: args,
+			}
+
+			inst, err := CreateInstance("386", image, &opts)
+			errStr := "additional argument id must greater than 64"
+			require.EqualError(t, err, errStr)
+			require.Nil(t, inst)
+		})
+	})
+
+	t.Run("invalid version", func(t *testing.T) {
+
+	})
+
+	t.Run("invalid payload", func(t *testing.T) {
+
+	})
+
+}
+
+func TestEncodeVersion(t *testing.T) {
+
+}
