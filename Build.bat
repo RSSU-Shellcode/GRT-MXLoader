4299c979988cee6c644602e3fcf8ebd463b0fb22
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-09-06 13:47:12 +0800

    update generate builder and extract loader template for dotnet.

--- Build.bat
+++ Build.bat
@@ -15,27 +15,41 @@ rd /S /Q "Release"
 rd /S /Q "x64"
 rd /S /Q "builder\cs-beacon\Release"
 rd /S /Q "builder\cs-beacon\x64"
+rd /S /Q "builder\dotnet\Release"
+rd /S /Q "builder\dotnet\x64"
 
 echo ==================== generate builder ====================
 MSBuild.exe GRT-MXLoader.sln /t:cs-beacon /p:Configuration=Release /p:Platform=x86
 MSBuild.exe GRT-MXLoader.sln /t:cs-beacon /p:Configuration=Release /p:Platform=x64
+MSBuild.exe GRT-MXLoader.sln /t:dotnet    /p:Configuration=Release /p:Platform=x86
+MSBuild.exe GRT-MXLoader.sln /t:dotnet    /p:Configuration=Release /p:Platform=x64
 
 echo ================ extract loader template =================
 cd builder\cs-beacon
-echo --------extract template for x86--------
+echo --------extract cs-beacon template for x86--------
 "..\..\Release\cs-beacon.exe"
-echo --------extract template for x64--------
+echo --------extract cs-beacon template for x64--------
 "..\..\x64\Release\cs-beacon.exe"
 cd ..\..
 
+cd builder\dotnet
+echo --------extract dotnet template for x86--------
+"..\..\Release\dotnet.exe"
+echo --------extract dotnet template for x64--------
+"..\..\x64\Release\dotnet.exe"
+cd ..\..
+
 echo ================= copy standard template =================
-copy /Y dist\standard\CS_Beacon_*.bin loader\beacon\template
+copy /Y dist\standard\CS_Beacon_*.bin loader\cs-beacon\template
+copy /Y dist\standard\Dotnet_*.bin    loader\dotnet\template
 
 echo =================== clean build files ====================
 rd /S /Q "Release"
 rd /S /Q "x64"
 rd /S /Q "builder\cs-beacon\Release"
 rd /S /Q "builder\cs-beacon\x64"
+rd /S /Q "builder\dotnet\Release"
+rd /S /Q "builder\dotnet\x64"
 
 echo =================== test loader package ==================
 call test.bat
